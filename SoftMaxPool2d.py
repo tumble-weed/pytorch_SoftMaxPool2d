531a316a181f09e95469f55766417d65d211429e
tumble-weed <aniketsinghresearch@gmail.com>
2020-06-22 14:07:06 +0530

    added padding (required for resnet)

--- SoftMaxPool2d.py
+++ SoftMaxPool2d.py
@@ -1,15 +1,18 @@
 import torch
 
 class SoftMaxPool2d(torch.nn.Module):
-    def __init__(self,T,wsize,wstep):
+    def __init__(self,T,wsize,wstep,padding=0):
         assert len(wsize) == 2
         super(SoftMaxPool2d,self).__init__()
         self.T = torch.tensor(T).float()
         
         self.wsize = wsize
         self.wstep = wstep 
-
+        self.padding = padding
     def forward(self,t):
+        if self.padding > 0:
+            padder = torch.nn.ZeroPad2d(self.padding)
+            t = padder(t)
         assert t.dim() == 4
         u = t.unfold(2,self.wsize[0],self.wstep).unfold(3,self.wsize[1],self.wstep)
         w = u.contiguous().view(u.shape[:4] + (-1,))
